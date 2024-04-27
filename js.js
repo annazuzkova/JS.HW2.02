c585664fdbd2a8bf13e18c4238bae5769e64b283
AnnaZuzkova <famzu2018@gmail.com>
2024-04-27 17:53:04 +0300

    2

---
+++ js.js
@@ -0,0 +1,48 @@
+"use strict";
+const celsius = 15;
+const fahrenheit = (celsius * 9) / 5 + 32;
+console.log(fahrenheit);
+
+// 2
+
+const mounst = 31;
+const hours = mounst * 24;
+const minutes = hours * 60;
+console.log(hours, minutes);
+
+//3
+
+const health = 100;
+const enerdy = 100;
+console.log(health - 10, enerdy - 90);
+
+//4
+
+const fair = 100;
+const procent = fair / 100;
+console.log(fair - procent * 10);
+
+//5
+
+const number = 129445.3493395;
+console.log(Math.floor(number));
+
+//6
+const myNumber = "2189403.134879ewjfjhfwhr";
+console.log(Number.parseFloat(myNumber));
+
+//7
+
+const someNumber = "437djkhbcu";
+console.log(Number.parseInt(someNumber));
+
+//8
+
+const sgrtNumber = 144;
+console.log(Math.sqrt(sgrtNumber));
+
+//9
+
+const abra_kadabra = "24356789ergdfthygjuhkijlk";
+const abra_kadabra_number = Number.parseFloat(abra_kadabra);
+console.log(abra_kadabra_number.toString);
